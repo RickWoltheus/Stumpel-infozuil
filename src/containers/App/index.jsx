92c48c37c45eca8d38e391fecf3dbc5d1aa6b6dd
Rick Woltheus <rwoltheus@gmail.com>
2019-03-07 13:23:35 +0100

    feat(app): add appProvider

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,34 +1,55 @@
 import React, { Component } from 'react';
-import { Router ,Route, Link , Switch } from 'react-router-dom'
+import { Route, Link, Switch } from 'react-router-dom'
 
 import Home from '../Home'
 import NotFound from '../NotFound'
 
 import './style.scss';
 
+const defaultAppState = {
+  temp: 1
+}
+
+export const AppContext = React.createContext(defaultAppState);
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      ...defaultAppState,
+      changeTemp: this.handleChangeTemp
+    }
+  }
 
   render() {
     return (
-      <div className="App">
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
+      <AppContext.provider value={this.state}>
+        <div className="App">
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
 
-        <main>
+          <main>
             <Switch>
               <Route strict exact path="/" component={Home} />
               <Route component={NotFound} />
             </Switch>
-        </main>
-      </div>
+          </main>
+        </div>
+      </AppContext.provider>
+
     );
+  }
+
+  handleChangeTemp = () => {
+    this.setState({ temp: this.props.state + 1 })
   }
 }
 
