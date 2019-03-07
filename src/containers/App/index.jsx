41ef766c6c32f797cb87e62df3e453e0adee6e1d
Quenten Brouwer <quenten@uncinc.nl>
2019-03-07 14:07:41 +0100

    HOTFIX

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,79 +1,55 @@
-import React, { Component } from 'react'
-import { Route, Link, Switch } from 'react-router-dom'
 import React, { Component } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
 
 import Home from '../Home';
 import NotFound from '../NotFound';
 
-import './style.scss'
+import './style.scss';
 
 const defaultAppState = {
-    temp: 1,
-}
+  temp: 1,
+};
 
-export const AppContext = React.createContext(defaultAppState)
+export const AppContext = React.createContext(defaultAppState);
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            ...defaultAppState,
-            changeTemp: this.handleChangeTemp,
-        }
-    }
-
-    render() {
-        return (
-            <AppContext.Provider value={this.state}>
-                <div className="App">
-                    <header>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <Link to="/">Home</Link>
-                                </li>
-                            </ul>
-                        </nav>
-                    </header>
-
-                    <main>
-                        <Switch>
-                            <Route strict exact path="/" component={Home} />
-                            <Route component={NotFound} />
-                        </Switch>
-                    </main>
-                </div>
-            </AppContext.Provider>
-        )
-    }
-
-
-    handleChangeTemp = () => {
-        this.setState({ temp: this.state.temp + 1 })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...defaultAppState,
+      changeTemp: this.handleChangeTemp,
+    };
+  }
 
   render() {
     return (
-      <div className="App">
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
+      <AppContext.Provider value={this.state}>
+        <div className="App">
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
 
-        <main>
-          <Switch>
-            <Route strict exact path="/" component={Home} />
-            <Route component={NotFound} />
-          </Switch>
-        </main>
-      </div>
+          <main>
+            <Switch>
+              <Route strict exact path="/" component={Home} />
+              <Route component={NotFound} />
+            </Switch>
+          </main>
+        </div>
+      </AppContext.Provider>
     );
   }
+
+
+    handleChangeTemp = () => {
+      this.setState({ temp: this.state.temp + 1 });
+    }
 }
 
-export default App
+export default App;
