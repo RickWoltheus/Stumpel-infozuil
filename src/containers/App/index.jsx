3b74b35139ddeaeead1c7242484262df050086ce
Rick Woltheus <rwoltheus@gmail.com>
2019-03-07 13:47:03 +0100

    feat(*): add context and prettier

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,56 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Route, Link, Switch } from 'react-router-dom'
 
 import Home from '../Home'
 import NotFound from '../NotFound'
 
-import './style.scss';
+import './style.scss'
 
 const defaultAppState = {
-  temp: 1
+    temp: 1,
 }
 
-export const AppContext = React.createContext(defaultAppState);
+export const AppContext = React.createContext(defaultAppState)
 
 class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      ...defaultAppState,
-      changeTemp: this.handleChangeTemp
+    constructor(props) {
+        super(props)
+        this.state = {
+            ...defaultAppState,
+            changeTemp: this.handleChangeTemp,
+        }
     }
-  }
 
-  render() {
-    return (
-      <AppContext.provider value={this.state}>
-        <div className="App">
-          <header>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-              </ul>
-            </nav>
-          </header>
+    render() {
+        return (
+            <AppContext.Provider value={this.state}>
+                <div className="App">
+                    <header>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
 
-          <main>
-            <Switch>
-              <Route strict exact path="/" component={Home} />
-              <Route component={NotFound} />
-            </Switch>
-          </main>
-        </div>
-      </AppContext.provider>
+                    <main>
+                        <Switch>
+                            <Route strict exact path="/" component={Home} />
+                            <Route component={NotFound} />
+                        </Switch>
+                    </main>
+                </div>
+            </AppContext.Provider>
+        )
+    }
 
-    );
-  }
-
-  handleChangeTemp = () => {
-    this.setState({ temp: this.props.state + 1 })
-  }
+    handleChangeTemp = () => {
+        this.setState({ temp: this.state.temp + 1 })
+    }
 }
 
-export default App;
+export default App
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { AppContext } from './../App/index';
 
 class Home extends Component {
-    render() {
-        return (
-          <div>Home</div>
-        );
-    }
+  render() {
+    return (
+
+      <AppContext.Consumer>
+        {({ temp, changeTemp }) => (
+
+          <div>Home
+
+            <button onClick={changeTemp}>count {temp} </button>
+          </div>
+
+        )}
+      </AppContext.Consumer>
+
+    );
+  }
 }
 
 export default Home;
