38bfba62b252eac6769f486fd6a748b0ce5a123d
Quenten Brouwer <quenten@uncinc.nl>
2019-03-07 10:57:48 +0100

    feat(router): add react router to project / change container structure

---
+++ src/containers/App/index.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Router ,Route, Link , Switch } from 'react-router-dom'
+
+import Home from '../Home'
+import NotFound from '../NotFound'
+
+import './style.scss';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <main>
+            <Switch>
+              <Route strict exact path="/" component={Home} />
+              <Route component={NotFound} />
+            </Switch>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
